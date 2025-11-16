477c32f4d610de414264045e0e296a54eb42e0fa
Leonardo Vanzei <leonardo.vanzei123@gmail.com>
2025-11-16 15:07:19 -0500

    codecrafters submit [skip ci]

--- app/builtin/lrange.go
+++ app/builtin/lrange.go
@@ -16,13 +16,27 @@ func HandleLrange(elements []string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("Start Index not a number")
 	}
+	if startIdx < 0 {
+		startIdx += len(dbK.List)
+	}
+	if startIdx < 0 {
+		startIdx = 0
+	}
 	endIdx, err := strconv.Atoi(elements[3])
 	if err != nil {
 		return "", fmt.Errorf("End Index not a number")
 	}
+	if endIdx < 0 {
+		endIdx += len(dbK.List)
+	}
 	if (endIdx) > len(dbK.List) {
 		endIdx = len(dbK.List) - 1
 	}
+
+	if startIdx > endIdx || startIdx >= len(dbK.List) {
+		return "*0\r\n", nil
+	}
+	fmt.Println(startIdx, endIdx)
 
 	result := dbK.List[startIdx : endIdx+1]
 
